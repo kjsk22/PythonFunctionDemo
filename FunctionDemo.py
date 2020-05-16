57f3b2000a71099516cfd09bd84e20653b5e80ee
Santhoshkumar Kobula <santhoshkumar_kj@yahoo.com>
2020-05-16 19:47:27 +0530

    Update FunctionDemo.py

--- FunctionDemo.py
+++ FunctionDemo.py
@@ -1,12 +1,12 @@
-def factorial(n):
+def factorial(n): #calculates the factorial for n
     result = 1
-    while n > 0:
+    while n > 0: # repeat this until n becomes 1
         a = n
         result = result * a
         n -= 1
     return result
     
-def cylindervolume(radius, height):
+def cylindervolume(radius, height): # take radius and height and calculate volume of cylinder using forumla Pi*(r)^2*h
     volume = 3.14 * (radius ** 2) * height
     return volume
 
